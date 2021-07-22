c57b65230268f8afabccff81b442a1a62ae3bc3b
Luvpreet Singh <s.luvpreet33@gmail.com>
2021-07-22 22:12:44 +0530

    add debug statement to get event names

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh -l
 
 IMAGE_NAME="$INPUT_GCR_HOST/$GOOGLE_PROJECT_ID/$REPOSITORY_NAME/$INPUT_IMAGE_NAME:$GITHUB_SHA"
+echo $GITHUB_EVENT_NAME
+echo $GITHUB_SHA
+echo $GITHUB_REF
 echo $INPUT_IMAGE_NAME
 echo $IMAGE_NAME
 
 echo $GCLOUD_SERVICE_KEY | docker login -u _json_key --password-stdin https://$INPUT_GCR_HOST/
-docker build -t $IMAGE_NAME .
-docker push $IMAGE_NAME
+#docker build -t $IMAGE_NAME .
+#docker push $IMAGE_NAME
